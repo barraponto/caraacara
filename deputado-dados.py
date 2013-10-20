64a7205482f060cd635088999cefda669fc911ab
Capi Etheriel <barraponto@gmail.com>
2013-10-20 13:33:45 -0200

    getting good data from almg webservices

---
+++ deputado-dados.py
@@ -0,0 +1,49 @@
+import json
+from urllib.request import urlopen
+from itertools import chain
+
+
+def get_data(json_url):
+    with urlopen(json_url) as response:
+        return json.loads(response.readall().decode('utf-8'))
+
+
+def get_lideres(json_url):
+    lideranca = get_data(json_url)
+    return [unidade['lider']['id']
+            for unidade in lideranca['list']]
+
+
+exercicio_url = ('http://dadosabertos.almg.gov.br/'
+                 'ws/deputados/em_exercicio?formato=json')
+deputado_url = ('http://dadosabertos.almg.gov.br/'
+                'ws/deputados/{0!s}?formato=json')
+comissao_url = ('http://dadosabertos.almg.gov.br/'
+                'ws/deputados/18847?formato=json')
+
+exercicio = get_data(exercicio_url)
+deputados = {deputado['id']:
+             get_data(deputado_url.format(deputado['id']))['deputado']
+             for deputado in exercicio['list']}
+
+liderancas = [('http://dadosabertos.almg.gov.br/ws/'
+               'representacao_partidaria/blocos/liderancas?formato=json'),
+              ('http://dadosabertos.almg.gov.br/ws/'
+               'representacao_partidaria/bancadas/liderancas?formato=json'),
+              ('http://dadosabertos.almg.gov.br/ws/'
+               'representacao_partidaria/governo/liderancas?formato=json'),
+              ('http://dadosabertos.almg.gov.br/ws/'
+               'representacao_partidaria/maioria/liderancas?formato=json'),
+              ('http://dadosabertos.almg.gov.br/ws/'
+               'representacao_partidaria/minoria/liderancas?formato=json')]
+
+lideres = list(chain(*[get_lideres(url) for url in liderancas]))
+import pprint; pprint.pprint(lideres)
+
+data = [{'nome': deputado['nome'],
+         'partido': deputado['partido'],
+         'legislaturas': len(deputado['legislaturas'])}
+        for deputado in deputados.values()]
+
+with open('data.json', 'w') as output:
+    json.dump(data, output)
