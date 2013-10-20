fb7bc0c19251d23ab7576b6eb9237a7de7c51d6d
Capi Etheriel <barraponto@gmail.com>
2013-10-20 14:49:46 -0200

    applied tags to people css classes

---
+++ public/js/app/templates/helpers/tagger.js
@@ -0,0 +1,11 @@
+define(['handlebars'], function (Handlebars){
+  function tagger(context, options) {
+    var tags = [];
+    tags.push(context['partido']);
+    tags.push(context['legislaturas'] + '-legislaturas');
+    return tags.join(' ');
+  }
+
+  Handlebars.registerHelper('tagger', tagger);
+  return tagger;
+});
