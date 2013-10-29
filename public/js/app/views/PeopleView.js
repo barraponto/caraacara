f0bce8dbee97c49812f161c08013f494e58d8d95
Capi Etheriel <barraponto@gmail.com>
2013-10-29 19:38:18 -0200

    fix the ugly person template

--- public/js/app/views/PeopleView.js
+++ public/js/app/views/PeopleView.js
@@ -2,12 +2,8 @@ define( ['App', 'backbone', 'marionette', 'jquery', 'views/PersonView', 'hbs!tem
     function(App, Backbone, Marionette, $, PersonView, template) {
         //ItemView provides some default rendering logic
         return Backbone.Marionette.CollectionView.extend({
+            tagName: 'ul',
             template: template,
             itemView: PersonView,
-            itemViewContainer: 'ul',
-            tagName: 'div',
-            // View Event Handlers
-            events: {
-            }
         });
     });
